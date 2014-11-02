cca839793830619b665a560f7e878b0dc213a7ac
sintyaj <sintyaj@gmail.com>
2014-11-02 16:08:39 +0200

    Android test application

---
+++ test/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/appsflyer/Workspace/af-test/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/appsflyer/Workspace/af-test/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ test/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/appsflyer/Workspace/af-test/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/appsflyer/Workspace/af-test/platforms/android/AndroidManifest.xml \
